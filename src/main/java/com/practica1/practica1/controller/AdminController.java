bb3691f4ff4406b84e4005fb87615a1446eb3587
aaronblanco <aaronba13@hotmail.com>
2025-10-15 21:40:51 +0200

    fix edit admin.

--- src/main/java/com/practica1/practica1/controller/AdminController.java
+++ src/main/java/com/practica1/practica1/controller/AdminController.java
@@ -1,7 +1,8 @@
 package com.practica1.practica1.controller;
 
-import com.practica1.practica1.service.DatabaseExportService;
-import com.practica1.practica1.service.ProductoService;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,9 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.practica1.practica1.model.Producto;
+import com.practica1.practica1.service.DatabaseExportService;
+import com.practica1.practica1.service.ProductoService;
 
 @Controller
 public class AdminController {
@@ -19,14 +22,21 @@ public class AdminController {
     private final ProductoService productoService;
     private final DatabaseExportService exportService;
 
-    public AdminController(ProductoService productoService,
-            DatabaseExportService exportService) {
+    public AdminController(ProductoService productoService, DatabaseExportService exportService) {
         this.productoService = productoService;
         this.exportService = exportService;
     }
 
     @GetMapping("/admin")
-    public String admin(Model model) {
+    public String adminPage(Model model) {
+        model.addAttribute("product", new Producto());
+        model.addAttribute("products", productoService.getAllProducts());
+        return "admin";
+    }
+
+    @GetMapping("/admin/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productoService.getProductById(id));
         model.addAttribute("products", productoService.getAllProducts());
         return "admin";
     }
@@ -34,7 +44,6 @@ public class AdminController {
     @GetMapping("/admin/download-db-sql")
     public ResponseEntity<ByteArrayResource> downloadDbSql() {
         byte[] data = exportService.exportDatabaseToSql();
-
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
         String filename = "productos-" + timestamp + ".sql";
 
